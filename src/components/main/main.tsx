e6edd491aacc92ced536d16dbe12639fa24cecb2
Zoom <egor147536987@gmail.com>
2024-05-18 00:38:39 +0300

    Remove fullscreen toggle logic in Main component.

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -73,11 +73,6 @@ const Main: FC<IMain> = ({ pathRest }) => {
         setCollapse(true)
         flag = true
       } else if (window.innerWidth >= 768 && flag) {
-        if (document.fullscreenElement != null) {
-          void document.exitFullscreen()
-        } else {
-          void document.body.requestFullscreen()
-        }
         setCollapse(false)
         flag = false
       }
