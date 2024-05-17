9dcd7e94bae81e37a56e6f008d990d15c99fe22d
Zoom <egor147536987@gmail.com>
2024-05-18 00:51:37 +0300

    "Remove fullscreen toggle on window resize"

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -65,11 +65,6 @@ const Main: FC<IMain> = ({ pathRest }) => {
   if (typeof window !== 'undefined') {
     window.addEventListener('resize', function resizeHandler() {
       if (window.innerWidth < 768 && !flag) {
-        if (document.fullscreenElement != null) {
-          void document.exitFullscreen()
-        } else {
-          void document.body.requestFullscreen()
-        }
         setCollapse(true)
         flag = true
       } else if (window.innerWidth >= 768 && flag) {
