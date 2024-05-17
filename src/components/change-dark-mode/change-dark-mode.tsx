9140cde07c121ec423a441d30b7c962828fe956c
Zoom <egor147536987@gmail.com>
2024-05-17 22:57:37 +0300

    "Updated package-lock.json and package.json, added imports and functionality to ChangeDarkMode component"

--- src/components/change-dark-mode/change-dark-mode.tsx
+++ src/components/change-dark-mode/change-dark-mode.tsx
@@ -1,12 +1,23 @@
+import { Button } from 'antd'
 import React, { FC } from 'react'
+import { SunOutlined, MoonOutlined } from '@ant-design/icons'
 
 interface IChangeLanguage {
   dark: boolean
   changeDark: () => void
 }
 const ChangeDark: FC<IChangeLanguage> = ({ dark, changeDark }) => {
-  React.useEffect(() => {}, [])
+  React.useEffect(() => {
+    console.log(dark)
+  }, [dark])
 
-  return <div>AAA</div>
+  return (
+    <Button
+      icon={dark ? <MoonOutlined /> : <SunOutlined />}
+      onClick={() => changeDark()}
+    >
+      Search
+    </Button>
+  )
 }
 export default ChangeDark
