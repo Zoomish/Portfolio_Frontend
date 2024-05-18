b35eb8ce616c8e800d656cc4a0e5f4d27cb3c4e4
Zoom <egor147536987@gmail.com>
2024-05-18 21:54:28 +0300

    "Update image styling in Project component"

--- src/pages/project/project.tsx
+++ src/pages/project/project.tsx
@@ -17,7 +17,7 @@ const Project: FC<IRest> = ({ t, projects }) => {
   return (
     <div className='w-full h-full flex flex-col justify-start items-center mt-20 z-10'>
       <p className='text-3xl'>{project?.title}</p>
-      <img src={project?.image} className='w-96 h-60'></img>
+      <img src={project?.image} className='w-[450px] h-60 object-contain'></img>
     </div>
   )
 }
