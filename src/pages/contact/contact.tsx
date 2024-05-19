aeb16d34515f013a6553245679e802a1cd47574b
Zoom <egor147536987@gmail.com>
2024-05-19 16:36:12 +0300

    "Adjusted contact page layout: changed div heights, added text-center class to paragraphs, and updated font sizes."

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -20,34 +20,34 @@ const Contact: FC<IContact> = ({ pathRest, t, user }) => {
         {t('my_contacts')}
       </p>
       <div className='flex justify-center items-center flex-wrap w-full h-full relative mb-40'>
-        <div className='sm:w-1/2 w-full h-40 flex items-center flex-col'>
-          <p className='text-xl font-normal'>{t('email-address')}</p>
-          <p className='text-base mt-3'>
+        <div className='sm:w-1/2 w-full h-32 flex items-center flex-col'>
+          <p className='text-xl font-normal text-center'>{t('email-address')}</p>
+          <p className='text-base mt-3 text-center'>
             <a href={`mailto:${user.email}?subject=Job`}>{user.email}</a>
           </p>
         </div>
-        <div className='sm:w-1/2 w-full h-40 flex items-center flex-col'>
-          <p className='text-xl font-normal'>{t('contact-phone-number')}</p>
-          <p className='text-base mt-3'>{user.phone}</p>
+        <div className='sm:w-1/2 w-full h-32 flex items-center flex-col'>
+          <p className='text-xl font-normal text-center'>{t('contact-phone-number')}</p>
+          <p className='text-base mt-3 text-center'>{user.phone}</p>
         </div>
-        <div className='sm:w-1/2 w-full h-40 flex items-center flex-col'>
-          <p className='text-xl font-normal'>{t('telegram')}</p>
+        <div className='sm:w-1/2 w-full h-32 flex items-center flex-col'>
+          <p className='text-xl font-normal text-center'>{t('telegram')}</p>
           <a
             target='_blank'
             rel='noreferrer'
             href={`https://t.me/${user.tg}`}
-            className='text-base mt-3'
+            className='text-base mt-3 text-center'
           >
             {user.tg}
           </a>
         </div>
-        <div className='sm:w-1/2 w-full h-40 flex items-center flex-col'>
-          <p className='text-xl font-normal'>{t('telegram-bot')}</p>
+        <div className='sm:w-1/2 w-full h-32 flex items-center flex-col'>
+          <p className='text-xl font-normal text-center'>{t('telegram-bot')}</p>
           <a
             target='_blank'
             rel='noreferrer'
             href={`https://t.me/${user.tg_bot}`}
-            className='text-base mt-3'
+            className='text-base mt-3 text-center'
           >
             {user.tg_bot}
           </a>
