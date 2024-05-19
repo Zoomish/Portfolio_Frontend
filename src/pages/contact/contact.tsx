d292dc68ac59a889c7cb0c453912730d65fffc9c
Zoom <egor147536987@gmail.com>
2024-05-19 16:17:07 +0300

    Added "telegram" translation key to en, kz, and ru locales; updated contact page to display email, phone number, Telegram, and Telegram bot information.

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -22,15 +22,21 @@ const Contact: FC<IContact> = ({ pathRest, t, user }) => {
       <div className='flex justify-center items-center flex-wrap w-full h-full relative mb-40'>
         <div className='sm:w-1/2 w-full h-40 flex items-center flex-col'>
           <p className='text-xl font-normal'>{t('email-address')}</p>
+          <p className='text-base mt-3'>
+            <a href={`mailto:${user.email}?subject=Job`}>{user.email}</a>
+          </p>
         </div>
         <div className='sm:w-1/2 w-full h-40 flex items-center flex-col'>
-          <p className='text-xl font-normal'>{t('email-address')}</p>
+          <p className='text-xl font-normal'>{t('contact-phone-number')}</p>
+          <p className='text-base mt-3'>
+            <a href={`mailto:${user.email}?subject=Job`}>{user.email}</a>
+          </p>
         </div>
         <div className='sm:w-1/2 w-full h-40 flex items-center flex-col'>
-          <p className='text-xl font-normal'>{t('email-address')}</p>
+          <p className='text-xl font-normal'>{t('telegram')}</p>
         </div>
         <div className='sm:w-1/2 w-full h-40 flex items-center flex-col'>
-          <p className='text-xl font-normal'>{t('email-address')}</p>
+          <p className='text-xl font-normal'>{t('telegram-bot')}</p>
         </div>
       </div>
     </div>
