6202f2c69fa2148103e56ed9e3dcd944b0870576
Zoom <egor147536987@gmail.com>
2024-05-19 16:03:28 +0300

    "Updated Contact component with new layout and added four div elements with 'F' content"

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -15,8 +15,15 @@ const Contact: FC<IContact> = ({ pathRest, t }) => {
   }, [])
   return (
     <div className='flex flex-col justify-center  w-full h-full z-10'>
-      <p className='md:text-4xl text-2xl text-center mb-10'>{t('my_contacts')}</p>
-      <div className='flex justify-center flex-wrap w-full h-full gap-2 relative'></div>
+      <p className='md:text-4xl text-2xl text-center mb-10'>
+        {t('my_contacts')}
+      </p>
+      <div className='flex justify-center items-center flex-wrap w-full h-full relative mb-40'>
+        <div className='sm:w-1/2 w-full h-40'>F</div>
+        <div className='sm:w-1/2 w-full h-40'>F</div>
+        <div className='sm:w-1/2 w-full h-40'>F</div>
+        <div className='sm:w-1/2 w-full h-40'>F</div>
+      </div>
     </div>
   )
 }
