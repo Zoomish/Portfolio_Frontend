21ba76e0038af47b238584e1d9135afd20e4e1c7
Zoom <egor147536987@gmail.com>
2024-05-19 12:45:08 +0300

    "Refactored project page layout and added a second button with a different gradient style"

--- src/pages/project/project.tsx
+++ src/pages/project/project.tsx
@@ -18,23 +18,43 @@ const Project: FC<IRest> = ({ t, projects }) => {
   }, [location.pathname])
   return (
     <div className='w-full h-full flex flex-col justify-start items-center mt-20 z-10'>
-      <img src={project?.image} className='w-[450px] h-60 object-contain'></img>
-      <p className='text-3xl'>
-        {t('title')}: {project?.title}
-      </p>
-      <p className='text-xl'>
-        {t('description')}: {project?.description}
-      </p>
-      <p className='text-xl'>
-        {t('tehnologies')}:{' '}
-        {project?.tags
-          .replaceAll(' ', '')
-          .split(',')
-          .map((tag) => {
-            return tag + ' '
-          })}
-      </p>
-      <Button type="primary" icon={<EyeOutlined />} className='flex justify-center items-center'>{t('overview')}</Button>
+      <div className='flex flex-col justify-start items-center gap-1'>
+        <img
+          src={project?.image}
+          className='w-[450px] h-60 object-contain'
+        ></img>
+        <p className='text-3xl'>
+          {t('title')}: {project?.title}
+        </p>
+        <p className='text-xl'>
+          {t('description')}: {project?.description}
+        </p>
+        <p className='text-xl'>
+          {t('tehnologies')}:{' '}
+          {project?.tags
+            .replaceAll(' ', '')
+            .split(',')
+            .map((tag) => {
+              return tag + ' '
+            })}
+        </p>
+        <div className='flex justify-around w-full'>
+          <Button
+            type='primary'
+            icon={<EyeOutlined />}
+            className='flex justify-center items-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%'
+          >
+            {t('overview')}
+          </Button>
+          <Button
+            type='primary'
+            icon={<EyeOutlined />}
+            className='flex justify-center items-center bg-gradient-to-r from-emerald-500 from-10% via-sky-500 via-30% to-indigo-500 to-90%'
+          >
+            {t('overview')}
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
