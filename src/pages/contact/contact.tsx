eacf46927e817a965e80a30011d5b7dd833bf01b
Zoom <egor147536987@gmail.com>
2024-05-19 16:09:59 +0300

    "Changed font style from 'font-medium' to 'font-normal' for four paragraphs in Contact component."

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -21,16 +21,16 @@ const Contact: FC<IContact> = ({ pathRest, t, user }) => {
       </p>
       <div className='flex justify-center items-center flex-wrap w-full h-full relative mb-40'>
         <div className='sm:w-1/2 w-full h-40 flex items-center flex-col'>
-          <p className='text-xl font-medium'>{t('email-address')}</p>
+          <p className='text-xl font-normal'>{t('email-address')}</p>
         </div>
         <div className='sm:w-1/2 w-full h-40 flex items-center flex-col'>
-          <p className='text-xl font-medium'>{t('email-address')}</p>
+          <p className='text-xl font-normal'>{t('email-address')}</p>
         </div>
         <div className='sm:w-1/2 w-full h-40 flex items-center flex-col'>
-          <p className='text-xl font-medium'>{t('email-address')}</p>
+          <p className='text-xl font-normal'>{t('email-address')}</p>
         </div>
         <div className='sm:w-1/2 w-full h-40 flex items-center flex-col'>
-          <p className='text-xl font-medium'>{t('email-address')}</p>
+          <p className='text-xl font-normal'>{t('email-address')}</p>
         </div>
       </div>
     </div>
