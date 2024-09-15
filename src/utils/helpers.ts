bf7455a973f527e6598fb35c2ae7afe0ad6483d2
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-15 18:37:03 +0300

    "Added eslint disable comments and fixed newline at end of file in helpers.ts"

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable eqeqeq */
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const handleResponse = async (response: Response) => {
   if (response.ok) return await response.json()
     else if (response.status === 401) {
