7e45ca9c6817ac19d4deb15b2204ccf5182b902a
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-15 19:29:35 +0300

    Added line to store user data in local storage after API call.

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -69,6 +69,7 @@ const Main: FC<IMain> = ({ pathRest }) => {
             'deleteTime',
             String(+new Date() + 3600 * 1000 * 24)
           )
+          localStorage.setItem('user', JSON.stringify(res))
         })
         .catch((e) => openNotification(e, 'topRight'))
     }
